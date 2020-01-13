6db6c1cea06d0fdb9ede638bee7b614af3cf55e8
Matthew Johnson <matthew.dave.johnson@gmail.com>
2020-01-13 14:49:45 -0800

    Refactored prompt function and switch statement

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,27 +1,36 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to Calculator!');
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-console.log("What's the first number?");
+prompt('Welcome to Calculator!');
+
+prompt("What's the first number?");
 let number1 = readline.question();
 
-console.log("What's the second number?");
+prompt("What's the second number?");
 let number2 = readline.question();
 
-console.log(
+prompt(
   'What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide'
 );
 let operation = readline.question();
 
 let output;
-if (operation === '1') {
-  output = Number(number1) + Number(number2);
-} else if (operation === '2') {
-  output = Number(number1) - Number(number2);
-} else if (operation === '3') {
-  output = Number(number1) * Number(number2);
-} else if (operation === '4') {
-  output = Number(number1) / Number(number2);
+switch (operation) {
+  case '1':
+    output = Number(number1) + Number(number2);
+    break;
+  case '2':
+    output = Number(number1) - Number(number2);
+    break;
+  case '3':
+    output = Number(number1) * Number(number2);
+    break;
+  case '4':
+    output = Number(number1) / Number(number2);
+    break;
 }
 
-console.log(`The result is: ${output}`);
+prompt(`The result is: ${output}`);
