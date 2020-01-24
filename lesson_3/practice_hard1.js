74ebb3f0d9ef38323cc292b3dcff6f7d34d35934
Matthew Johnson <matthew.dave.johnson@gmail.com>
2020-01-24 13:56:15 -0800

    Select Practice Problems: Hard 1 completed

---
+++ lesson_3/practice_hard1.js
@@ -0,0 +1,42 @@
+// Question 4: Fix the code to return a false condition and handle
+// the case when the input string has more or less than 4 components.
+
+// Original code
+/**************************
+function isDotSeparatedIpAddress(inputString) {
+  let dotSeparatedWords = inputString.split(".");
+  while (dotSeparatedWords.length > 0) {
+    let word = dotSeparatedWords.pop();
+    if (!isAnIpNumber(word)) {
+      break;
+    }
+  }
+
+  return true;
+}
+***************************/
+
+// Fixed code
+
+function isAnIpNumber(str) {
+  if (!/^\d+$/.test(str)) return false;
+
+  let number = Number(str);
+  return number >= 0 && number <= 255;
+}
+
+function isDotSeparatedIpAddress(inputString) {
+  let dotSeparatedWords = inputString.split('.');
+  if (dotSeparatedWords.length !== 4) {
+    return false;
+  }
+
+  while (dotSeparatedWords.length > 0) {
+    let word = dotSeparatedWords.pop();
+    if (!isAnIpNumber(word)) {
+      return false;
+    }
+  }
+
+  return true;
+}
